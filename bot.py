ae516c554a4e7081ceae9bd7ca5e5df84f989b49
chikpikmik <chikpikmik@gmail.com>
2026-02-15 18:02:39 +0300

    Refactor bot.py to comment out local Bot API session initialization. The session setup is now disabled, maintaining the existing bot initialization for remote API usage.

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 from setting import TOKEN, USE_LOCAL_BOT_API, LOCAL_BOT_API_BASE
 
 from aiogram import Bot
-from aiogram.client.session import AiohttpSession
-from aiogram.client.telegram import TelegramAPIServer
+#from aiogram.client.session import AiohttpSession
+#from aiogram.client.telegram import TelegramAPIServer
 
 if USE_LOCAL_BOT_API:
-    session = AiohttpSession(
-        api=TelegramAPIServer.from_base(LOCAL_BOT_API_BASE)
-    )
-    bot = Bot(token=TOKEN, session=session)
+    pass
+    #session = AiohttpSession(
+    #    api=TelegramAPIServer.from_base(LOCAL_BOT_API_BASE)
+    #)
+    #bot = Bot(token=TOKEN, session=session)
 else:
     bot = Bot(token=TOKEN)
